d13e667e202583959de97fcb9b1345201195da8c
sigu <sigu@podiihq.com>
2019-04-25 10:40:17 +0300

    copy config

--- config/config.exs
+++ config/config.exs
@@ -3,28 +3,35 @@
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
-
-# General application configuration
 use Mix.Config
 
+# General application configuration
 config :distillery_example,
-  ecto_repos: [DistilleryExample.Repo]
+  ecto_repos: [Example.Repo]
 
 # Configures the endpoint
-config :distillery_example, DistilleryExampleWeb.Endpoint,
+config :distillery_example, ExampleWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "g1WSm3O2UlUaHAy3S/0txtJ0QFQU+d/cxfNnhXNKx85a93jSsYc/eg2T95XLsh4W",
-  render_errors: [view: DistilleryExampleWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: DistilleryExample.PubSub, adapter: Phoenix.PubSub.PG2]
+  secret_key_base: "WLTAq0m3cyhaqufuub/5RyX8E8/s2UR2P1cVJVWn0d46GixiKO6yaTa6i8B8jbzS",
+  render_errors: [view: ExampleWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Example.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+config :phoenix, :json_library, Jason
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: [:user_id]
 
-# Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
+config :libcluster,
+  topologies: [
+    example: [
+      strategy: Cluster.Strategy.Epmd,
+      config: [hosts: []]
+    ]
+  ]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{Mix.env}.exs"
--- config/test.exs
+++ config/test.exs
@@ -2,15 +2,16 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :distillery_example, DistilleryExampleWeb.Endpoint,
-  http: [port: 4002],
+config :distillery_example, ExampleWeb.Endpoint,
+  http: [port: 4001],
   server: false
 
 # Print only warnings and errors during test
 config :logger, level: :warn
 
 # Configure your database
-config :distillery_example, DistilleryExample.Repo,
+config :distillery_example, Example.Repo,
+  adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "postgres",
   database: "distillery_example_test",
